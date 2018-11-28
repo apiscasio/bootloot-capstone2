6d1bddf64a8804ccdf2e9dc0b3dc7e0b089452d6
Abigail Piscasio <abigail.piscasio@gmail.com>
2018-11-28 16:31:00 +0800

    capstone2 day1

---
+++ app/views/index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="../assets/css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Start your project here-->
+  <main id="landing-page" class="container-fluid">
+    <div id="logoJumbo" class="jumbotron text-center m-0"> LOGO </div>
+    <nav class="navbar navbar-expand-lg navbar-light amber accent-2 sticky-top">
+
+      <!-- Navbar brand -->
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <!-- Collapse button -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Collapsible content -->
+      <div class="collapse navbar-collapse" id="basicExampleNav">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+
+          <!-- Dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+
+        </ul>
+        <!-- Links -->
+
+        <form class="form-inline">
+          <div class="md-form my-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          </div>
+        </form>
+      </div>
+    <!-- Collapsible content -->
+
+    </nav>
+    <!--/.Navbar-->
+
+<!-- ======================================================================= -->
+
+    <section class="row"> <!-- landing page section -->
+      <div class="col-12">
+        <div id="bgJumbo" class="jumbotron p-0 m-0">
+          <!-- Card image -->
+          <div class="view overlay rounded-top">
+            
+            <a href="#">
+              <div class="mask rgba-white-slight"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section> <!-- landing page section -->
+
+<!-- ======================================================================= -->
+
+    <h3> Featured Items </h3>
+    <section class="row"> <!-- featured items section -->
+
+        <div class="col-4">
+          <!-- Card Wider -->
+          <div class="card card-cascade wider">
+
+            <!-- Card image -->
+            <div class="view view-cascade overlay">
+              <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!-- Card content -->
+            <div class="card-body card-body-cascade text-center">
+
+              <!-- Title -->
+              <h4 class="card-title"><strong>Alison Belmont</strong></h4>
+              <!-- Subtitle -->
+              <h5 class="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+              <!-- Text -->
+              <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+              <!-- Linkedin -->
+              <a class="px-2 fa-lg li-ic"><i class="fa fa-linkedin"></i></a>
+              <!-- Twitter -->
+              <a class="px-2 fa-lg tw-ic"><i class="fa fa-twitter"></i></a>
+              <!-- Dribbble -->
+              <a class="px-2 fa-lg fb-ic"><i class="fa fa-facebook"></i></a>
+
+            </div>
+
+          </div>
+          <!-- Card Wider -->
+        </div>
+
+        <div class="col-4">
+          <!-- Card Wider -->
+          <div class="card card-cascade wider">
+
+            <!-- Card image -->
+            <div class="view view-cascade overlay">
+              <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!-- Card content -->
+            <div class="card-body card-body-cascade text-center">
+
+              <!-- Title -->
+              <h4 class="card-title"><strong>Alison Belmont</strong></h4>
+              <!-- Subtitle -->
+              <h5 class="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+              <!-- Text -->
+              <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+              <!-- Linkedin -->
+              <a class="px-2 fa-lg li-ic"><i class="fa fa-linkedin"></i></a>
+              <!-- Twitter -->
+              <a class="px-2 fa-lg tw-ic"><i class="fa fa-twitter"></i></a>
+              <!-- Dribbble -->
+              <a class="px-2 fa-lg fb-ic"><i class="fa fa-facebook"></i></a>
+
+            </div>
+
+          </div>
+          <!-- Card Wider -->
+        </div>
+
+        <div class="col-4">
+          <!-- Card Wider -->
+          <div class="card card-cascade wider">
+
+            <!-- Card image -->
+            <div class="view view-cascade overlay">
+              <img  class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!-- Card content -->
+            <div class="card-body card-body-cascade text-center">
+
+              <!-- Title -->
+              <h4 class="card-title"><strong>Alison Belmont</strong></h4>
+              <!-- Subtitle -->
+              <h5 class="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+              <!-- Text -->
+              <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+
+              <!-- Linkedin -->
+              <a class="px-2 fa-lg li-ic"><i class="fa fa-linkedin"></i></a>
+              <!-- Twitter -->
+              <a class="px-2 fa-lg tw-ic"><i class="fa fa-twitter"></i></a>
+              <!-- Dribbble -->
+              <a class="px-2 fa-lg fb-ic"><i class="fa fa-facebook"></i></a>
+
+            </div>
+
+          </div>
+          <!-- Card Wider -->
+        </div>
+
+
+
+
+    </section> <!-- featured items section -->
+
+  </main>
+  <!-- /Start your project here-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="../assets/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="../assets/js/mdb.min.js"></script>
+</body>
+
+</html>
